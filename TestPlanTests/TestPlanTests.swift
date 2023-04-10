44b281b071545c9a51ec99a2e39a190085162ea8
Emilien Stremsdoerfer <emstre@verily.com>
2023-04-10 11:07:28 -0700

    Initial commit

--- TestPlanTests/TestPlanTests.swift
+++ TestPlanTests/TestPlanTests.swift
@@ -18,7 +18,7 @@ final class TestPlanTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testUnitExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
@@ -26,7 +26,7 @@ final class TestPlanTests: XCTestCase {
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
-    func testPerformanceExample() throws {
+    func testUnitPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
