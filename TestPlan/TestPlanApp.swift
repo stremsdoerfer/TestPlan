db908a035c45837c477560703f3108def7b3389b
Emilien Stremsdoerfer <emstre@verily.com>
2023-04-10 10:27:30 -0700

    Initial Commit

---
+++ TestPlan/TestPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPlanApp.swift
+//  TestPlan
+//
+//  Created by Emilien Stremsdoerfer on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TestPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
